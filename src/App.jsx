290aceda25862e72fae530d85644105b42cdffc0
sithmi himanshi <sithmihimanshi@icloud.com>
2024-02-06 13:28:34 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
-  return <BrowserRouter>
+  return (
+   <BrowserRouter>
+   <Header/>
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/sign-in" element={<SignIn />} />
@@ -14,6 +17,7 @@ export default function App() {
     <Route path="/about" element={<About />} />
     <Route path="/profile" element={<Profile />} />
   </Routes>
-  </BrowserRouter>;
+  </BrowserRouter>
+  );
 }
 
